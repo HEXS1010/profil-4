b63af5af73afc5bf242452d9d21f3803ac975fe1
Komang Aswameda <aswameda18@gmail.com>
2026-08-18 19:47:17 +0800

    masih ada maslaah kita rehat paling besok lanjut

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import "./assets/font-awesome/css/all.min.css";
 import "./assets/component/Navbar.js";
 import "./assets/component/SideBar.js";
 import { initDotGrid } from "./assets/dot-grid.js";
+import { initLogoSlider } from "./assets/logo-slider.js";
 
 const heroWrapper = document.querySelector(".dot-grid-wrapper");
 if (heroWrapper) initDotGrid(heroWrapper);
+
+initLogoSlider("logoTrack");
