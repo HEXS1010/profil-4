2f2d21c8beecd81a681871067efcc39e23aaa126
Komang Aswameda <aswameda18@gmail.com>
2026-08-21 23:14:24 +0800

    perbaikan navbar

--- src/assets/component/Navbar.js
+++ src/assets/component/Navbar.js
@@ -1,46 +1,59 @@
 class Navbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <nav class="bg-white border-b-4 h-20 z-10">
+           <nav class="bg-white border-b-4 h-20 z-10">
         <div class="mx-auto w-full h-full max-w-7xl px-6">
           <div class="flex justify-between items-center h-full">
-             <div>
-              <h1 class="font-title font-bold text-2xl">
-                Protofolio saya
-              </h1>
-
+            <div>
+              <h1 class="font-title font-bold text-2xl">Protofolio saya</h1>
             </div>
 
-            <ul id="nav-menu" class="items-center gap-8 font-label hidden md:flex">
-              <li>
-                <a href="#" class="nav-link active focus:outline-none focus:ring-0"> Beranda </a>
+            <ul
+              id="nav-menu"
+              class="items-center font-label hidden md:flex h-full"
+            >
+              <li class="h-full">  
+                <a
+                  href="#"
+                  class="nav-link active focus:outline-none focus:ring-0"
+                >
+                  Beranda
+                </a>
               </li>
 
-              <li>
-                <a href="#" class="nav-link focus:outline-none focus:ring-0"> Tentang Saya </a>
+              <li class="h-full">
+                <a href="#" class="nav-link focus:outline-none focus:ring-0">
+                  Tentang Saya
+                </a>
               </li>
 
-              <li>
-                <a href="#" class="nav-link focus:outline-none focus:ring-0"> Projek </a>
+              <li class="h-full">
+                <a href="#" class="nav-link focus:outline-none focus:ring-0">
+                  Projek
+                </a>
               </li>
 
-              <li>
-                <a href="#" class="nav-link focus:outline-none focus:ring-0"> Skill </a>
+              <li class="h-full">
+                <a href="#" class="nav-link focus:outline-none focus:ring-0">
+                  Skill
+                </a>
               </li>
 
-              <li>
-                <a href="#" class="nav-link focus:outline-none focus:ring-0"> Kontak </a>
+              <li class="h-full">
+                <a href="#" class="nav-link focus:outline-none focus:ring-0">
+                  Kontak
+                </a>
               </li>
             </ul>
 
             <button
-                id="sidebar-toggle"
-                type="button"
-                class="text-xl md:hidden"
-                aria-label="Buka sidebar"
-              >
-                <i class="fa-solid fa-bars"></i>
-              </button>
+              id="sidebar-toggle"
+              type="button"
+              class="text-xl md:hidden"
+              aria-label="Buka sidebar"
+            >
+              <i class="fa-solid fa-bars"></i>
+            </button>
           </div>
         </div>
       </nav>
